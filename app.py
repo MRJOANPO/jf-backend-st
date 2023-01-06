fd6c56581710e9efd2e3ab77b5d6b74c1508df3e
MRJOANPO <jonas.powell@icloud.com>
2023-01-06 16:31:22 +0100

    updated for streamlit

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
 import streamlit as st
-from sqlalchemy import create_engine
+import mysql.connector
 import pandas as pd
 
 
-
-
-engine = create_engine(
-    url="{0}+{1}://{2}:{3}@{4}:{5}/{6}".format(
-        st.secrets["DATABASE_DIALECT"],
-        st.secrets["DATABASE_DRIVER"],
-        st.secrets["DATABASE_USER"],
-        st.secrets["DATABASE_PASSWORD"],
-        st.secrets["DATABASE_HOST"],
-        st.secrets["DATABASE_PORT"],
-        st.secrets["DATABASE_NAME"]
-    )
+connection = mysql.connector.connect(
+    host=st.secrets["DATABASE_HOST"],
+    port=st.secrets["DATABASE_PORT"],
+    user=st.secrets["DATABASE_USER"],
+    password=st.secrets["DATABASE_PASSWORD"],
+    database=st.secrets["DATABASE_NAME"]
 )
 
 query = "SELECT * FROM `Anmeldung`"
-data_total = pd.read_sql(query, engine)
+data_total = pd.read_sql(query, connection)
 
 st.dataframe(data_total, use_container_width=True)
 
